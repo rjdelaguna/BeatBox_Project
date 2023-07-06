b1140dc9a7ab3a85d092763e7167c1b20fb5c8ae
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-06 15:36:58 -0700

    Create node class and tests

---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+class Node
+    attr_reader :data
+    def initialize(data)
+        @data = data
+    end
+
+    def next_node
+        @next_node = nil
+    end
+end
---
+++ spec/node_spec.rb
@@ -0,0 +1,21 @@
+require "./lib/node"
+
+RSpec.describe Node do
+    it "exists" do
+        node = Node.new("plop")
+
+        expect(node).to be_an_instance_of(Node)
+    end
+
+    it "has readable attributes" do
+        node = Node.new("plop")
+
+        expect(node.data).to eq("plop")
+    end
+
+    it "identifies the next node as nil" do
+        node = Node.new("plop")
+
+        expect(node.next_node).to eq(nil)
+    end
+end
